bad1cda16c94743f0885b5a64257ef9d1618fcf3
jiangjiang <koueing@gmail.com>
2019-08-03 23:45:51 +0900

    if no cancel record

--- controller/HistoryController.py
+++ controller/HistoryController.py
@@ -131,19 +131,20 @@ def cancel():
     liquidApi = liquid.liquidApi()
     mysqlmodel = MysqlModel.MysqlModel()
     record = mysqlmodel.selectCancelRecord()
-    # sell
-    data = {
-        'order_type': 'market',
-        'product_id': 5,
-        'side': 'sell',
-        'quantity': float(record[0][2])
-    }
-    order = liquidApi.order(data)
-    if ('id' in order):
-        print(order)
-        print('7時間を越えたため売りました。' + str(order['price']))
-        mysqlmodel.updateOrderHistorySellType(record[0][1], 2)
-        mysqlmodel.insertSellHistory(record[0][1], order['id'], order['quantity'], order['price'])
+    if(len(record) > 0):
+        # sell
+        data = {
+            'order_type': 'market',
+            'product_id': 5,
+            'side': 'sell',
+            'quantity': float(record[0][2])
+        }
+        order = liquidApi.order(data)
+        if ('id' in order):
+            print(order)
+            print('7時間を越えたため売りました。' + str(order['price']))
+            mysqlmodel.updateOrderHistorySellType(record[0][1], 2)
+            mysqlmodel.insertSellHistory(record[0][1], order['id'], order['quantity'], order['price'])
     time.sleep(1)
 
 

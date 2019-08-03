1732c2177e614239b923fb7358b76b9e1b47dfdc
jiangjiang <koueing@gmail.com>
2019-08-03 23:07:14 +0900

    Revert "add cancel method"
    This reverts commit 687ddd5591da6fd6bce529d2c1502bf77919d1ed.

--- model/sqlmodel.py
+++ model/sqlmodel.py
@@ -1,55 +1,46 @@
-import datetime
-
 import MySQLdb
+
 import config.myconfig
 
-
-
 class sqlmodel:
-   def __init__(self):
-       self.sql_insert = 'INSERT INTO '
-       self.sql_update = 'UPDATE TABLE '
-       self.table_name = 'LQ_ORDER'
-       global mydb
-       mydb = MySQLdb.connect(
-           unix_socket=config.myconfig.unix_socket,
-           user=config.myconfig.user,
-           host=config.myconfig.host,
-           db=config.myconfig.db)
+    def __init__(self):
+        global mydb
+        mydb = MySQLdb.connect(
+            unix_socket=config.myconfig.unix_socket,
+            user=config.myconfig.user,
+            host=config.myconfig.host,
+            db=config.myconfig.db)
 
-   def selectOrderHistory(self):
-       #global mydb
-       cur = mydb.cursor()
-       sql = "select * from LQ_ORDER"
-       cur.execute(sql)
-       rows = cur.fetchall()
-       cur.close
-       mydb.close
-       return rows
+    def selectOrderHistory(self):
+        global mydb
+        cur = mydb.cursor()
+        sql = "select * from name_age_list"
+        cur.execute(sql)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close
+        mydb.close
 
-   def DB_activate(self, DBname):
-       sql = self.sql_DBactive + DBname
-       self.cur.execute(sql)
+    def DB_activate(self, DBname):
+        sql = self.sql_DBactive + DBname
+        self.cur.execute(sql)
 
-   def SELECT_Column(self, table_name, *input_column_name):
-       for i in range(len(input_column_name)):
-           if i == 0:
-               column_name = input_column_name[0]
-           else:
-               column_name = column_name + ',' + input_column_name[i]
-       sql = "SELECT " + column_name + " from " + table_name
-       self.cur.execute(sql)
-       return self.cur.fetchall()
-       # 注文（買い、売り）時のRESPONSE情報を登録
+    def SELECT_Column(self, table_name, *input_column_name):
+        for i in range(len(input_column_name)):
+            if i == 0:
+                column_name = input_column_name[0]
+            else:
+                column_name = column_name + ',' + input_column_name[i]
 
+        sql = "SELECT " + column_name + " from " + table_name
+        self.cur.execute(sql)
 
-   def updateOrderHistorySell(self, orderIdSell, orderId):
-       # カーソルを取得する。
-       cur = mydb.cursor()
-       sql = self.sql_update + \
-             self.table_name + \
-             ' SET ORDER_ID_SELL = %s, LAST_MODIFIED_BY_ID = %s, LAST_MODIFIED_DATE = now(), DELETE_FLAG = 1 WHERE ORDER_ID = %s '
-       update_data = (orderIdSell, 'jiang', orderId)
-       result = cur.execute(sql, update_data)
-       mydb.commit()
-       return result
+        return self.cur.fetchall()
+
+    def INSERT_City_ID_Name(self, city_data):
+        sql = self.sql_insert + \
+              self.table_name + \
+              " (ID, City, Country) VALUES (%s, %s, %s)"
+        self.cur.executemany(sql, city_data)
+        self.conn.commit()
